05dafd1336d546fe9d12feecb95198d17247906c
minchodang <minsu910725@gmail.com>
2024-12-24 12:18:41 +0900

    feat: prettier setting

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,27 +1,24 @@
 import Fastify from 'fastify';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-
-dotenv.config()
+dotenv.config();
 
 const server = Fastify({
-    logger:true
-})
-
+  logger: true,
+});
 
 const start = async () => {
-    try {
-await server.get('/', async (request, reply) => {
-    return 'hello world'
-})
-await server.listen({
-    port: 3000
-})
-} catch (err) {
-        server.log.error(err)
-        process.exit(1)
-    }
-}
+  try {
+    await server.get('/', async (request, reply) => {
+      return 'hello world';
+    });
+    await server.listen({
+      port: 3000,
+    });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
 
-start()
-
+start();
